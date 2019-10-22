a63699f5bb9916cbde64fde6c148a1e4c43ecdcb
pusokricsi <Daewoo12345>
2019-10-22 19:40:58 +0300

    1

--- app/src/main/java/com/example/scrumpoker/JoinSessionActivity.java
+++ app/src/main/java/com/example/scrumpoker/JoinSessionActivity.java
@@ -1,16 +1,17 @@
 package com.example.scrumpoker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class JoinSessionActivity extends AppCompatActivity {
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_join_session);
     }
 }
