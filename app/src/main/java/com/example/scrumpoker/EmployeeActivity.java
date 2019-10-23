82a241de4be1954127d61ddec18f012f0c002a0e
MozesKadar <56792153+MozesKadar@users.noreply.github.com>
2019-10-23 23:46:04 +0300

    Employee1

--- app/src/main/java/com/example/scrumpoker/EmployeeActivity.java
+++ app/src/main/java/com/example/scrumpoker/EmployeeActivity.java
@@ -3,6 +3,9 @@ package com.example.scrumpoker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class EmployeeActivity extends AppCompatActivity {
 
@@ -10,5 +13,36 @@ public class EmployeeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee);
+
+        final Button customButton1 = findViewById(R.id.custom_button1);
+
+
+        customButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(EmployeeActivity.this, "Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final Button customButton2 = findViewById(R.id.custom_button2);
+
+
+        customButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(EmployeeActivity.this, "Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        final Button customButton3 = findViewById(R.id.custom_button3);
+
+
+        customButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(EmployeeActivity.this, "Click", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
