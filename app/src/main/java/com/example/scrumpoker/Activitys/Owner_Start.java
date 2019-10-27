cc58598d69ffc4277dbc2187541cbaec00951ca4
Roly <pap_roland2004@icloud.com>
2019-10-27 20:49:43 +0200

    friss

---
+++ app/src/main/java/com/example/scrumpoker/Activitys/Owner_Start.java
@@ -0,0 +1,16 @@
+package com.example.scrumpoker.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.scrumpoker.R;
+
+public class Owner_Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner__start);
+    }
+}
