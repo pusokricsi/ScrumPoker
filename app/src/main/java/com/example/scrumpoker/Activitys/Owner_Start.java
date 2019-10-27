cdedbccfbbe50f7a3837a2e96a97d6209502c06b
pusokricsi <Daewoo12345>
2019-10-28 01:08:47 +0200

    Mukodik V2.4

--- app/src/main/java/com/example/scrumpoker/Activitys/Owner_Start.java
+++ app/src/main/java/com/example/scrumpoker/Activitys/Owner_Start.java
@@ -1,17 +1,18 @@
 package com.example.scrumpoker.Activitys;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.scrumpoker.Objects.FirebaseRealtimeDatabaseHelper;
 import com.example.scrumpoker.R;
 
 
@@ -20,32 +21,42 @@ public class Owner_Start extends AppCompatActivity {
     private EditText newquestionEditText;
     private Button sendButton;
     private TextView owner;
+    FirebaseRealtimeDatabaseHelper fbdb;
 
 @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_owner__start);
-       inicialize();
-       owner=findViewById(R.id.Owner);
-       Intent intent=getIntent();
-       String s1=intent.getStringExtra("ownerName");
-       owner.setText(s1);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_owner__start);
+    inicialize();
+    owner = findViewById(R.id.Owner);
+    Intent intent = getIntent();
+    String s1 = intent.getStringExtra("ownerName");
+    String s2 = intent.getStringExtra("sessionId");
+    fbdb = new FirebaseRealtimeDatabaseHelper("1");
 
-    FragmentManager manager=getSupportFragmentManager();
-    final FragmentTransaction question_t=manager.beginTransaction();
-    final Question_Fragmant m4=new Question_Fragmant();
+    //owner.setText(s1);
+
+    FragmentManager manager = getSupportFragmentManager();
+    final FragmentTransaction question_t = manager.beginTransaction();
+    final Question_Fragmant m4 = new Question_Fragmant();
 
     sendButton.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Bundle b2=new Bundle();
-            b2.putString("question",newquestionEditText.getText().toString());
+            Bundle b2 = new Bundle();
+            b2.putString("question", newquestionEditText.getText().toString());
             m4.setArguments(b2);
-            question_t.add(R.id.questionView,m4);
+            question_t.add(R.id.questionView, m4);
             question_t.commit();
         }
     });
+}
 
+
+    @SuppressLint("MissingSuperCall")
+    protected void onStart() {
+        super.onStart();
+        owner.setText(fbdb.getSession().getOwnerName());
     }
 
     private void inicialize()
