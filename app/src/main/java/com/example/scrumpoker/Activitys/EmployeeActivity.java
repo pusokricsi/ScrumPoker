c205b04831219cc04dd6eb33344138d4b87ca3b0
MozesKadar <56792153+MozesKadar@users.noreply.github.com>
2019-10-28 15:52:52 +0200

    Employee4dewqr

--- app/src/main/java/com/example/scrumpoker/Activitys/EmployeeActivity.java
+++ app/src/main/java/com/example/scrumpoker/Activitys/EmployeeActivity.java
@@ -9,12 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.scrumpoker.Objects.FirebaseRealtimeDatabaseHelper;
-import com.example.scrumpoker.Objects.Question;
 import com.example.scrumpoker.R;
 
 public class EmployeeActivity extends AppCompatActivity {
